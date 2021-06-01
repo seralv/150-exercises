db95198ff444b7d78efd0421ec089be6e9bdd37d
seralv <sergioalvaro.14@gmail.com>
2021-06-01 16:27:14 -0400

    Update

---
+++ 007.js
@@ -0,0 +1,9 @@
+
+
+for (let year = 2014; year < 2050; year++) {
+    let day = new Date(`${ year }-01-01`);
+    console.log(`day`, day)
+    // if ( day.Date() === 0 ) {
+    //     console.log(`El primero de enero de ${ year } fue en domingo`);
+    // }    
+}
