406ecb33d58776a01165131f11ee3c4a66a3474b
seralv <sergioalvaro.14@gmail.com>
2021-06-02 09:52:29 -0400

    Exercise 012

---
+++ 012.js
@@ -0,0 +1,6 @@
+console.log( document.URL );
+//This print the URl for the website
+
+console.log( document.URL );
+console.log(`URL`, document.URL);
+console.log( 'This site run on ', document.URL );
