6ee03569667637f2cfccadaab47438562e1f42e3
seralv <sergioalvaro.14@gmail.com>
2021-06-02 10:10:48 -0400

    Exercise 014

---
+++ 014.js
@@ -0,0 +1,11 @@
+let filename = 'other.java';
+console.log( filename.split('.').pop() );
+
+filename = 'another.php.ko.ki';
+console.log( filename.split('.') );
+
+filename = 'PRINTER';
+console.log( filename.split('') );
+
+filename = 'This is a phrase';
+console.log( filename.split(' ') );
