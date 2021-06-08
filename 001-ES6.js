6aa929bcfa833376f65affbe73258223ca93d5ed
seralv <sergioalvaro.14@gmail.com>
2021-06-08 18:21:13 -0400

    Update last

--- 001-ES6.js
+++ 001-ES6.js
@@ -1,1 +1,11 @@
-document.getElementById( 'app' ).innerHTML = '<button class="btn btn-primary">Hola</button>';
+var header = document.querySelector('header');
+
+header.addEventListener( 'click', function( e ) {
+    console.log( 'Has clickeado en ' + e.target.nodeName );
+    e.stopPropagation();
+} );
+
+document.addEventListener( 'click', ( e ) => {
+    console.log(`Has clickeado en el documento`);
+    e.stopPropagation();
+} );
